932d8af00288761456d1f7c0d4a3e80e398cfd70
Sameer Singh <Sameersingh11604@gmail.com>
2026-01-29 03:22:43 +0000

    Next datatypes done

---
+++ .devcontainer/01_basicsfile/datatypes_summary.js
@@ -0,0 +1,25 @@
+// Primitive data types
+const score= 100
+const scoreValue=100.3
+
+const isLoggedIn=false
+const outsideTemp=null
+let userEmail;
+
+const id=Symbol('123')
+const anotherId=Symbol('123')
+
+console.log(id)==anotherId;
+
+const heros=["shaktiman", "nagraj"];
+let myObj={
+    name: "hitesh",
+    age: 22
+}
+
+const myfunction=function(){
+    console.log("Hello World");
+
+}
+
+console.log(typeof outsideTemp);
