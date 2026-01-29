6a16535819f21979f7aaa916b2e1d50abf95955b
Sameer Singh <Sameersingh11604@gmail.com>
2026-01-29 21:09:16 +0000

    Reset Basics

---
+++ .devcontainer/01_basicsfile/06_nums_maths.js
@@ -0,0 +1,18 @@
+const score = 400
+console.log(score);
+
+const balance=new Number(100)
+console.log(balance);
+
+console.log(balance.toString().length);
+
+
+const otherNumber=23.8966
+
+console.log(otherNumber.toPrecision(3));
+
+const hundreds=100000
+console.log(hundreds.toLocalString)
+
+
+
